ba731ab45d4bf48715fd68f417abd3103127ec6a
Mateusz Pomagier <mpomagier@gmail.com>
2023-08-16 00:36:14 +0200

    INIT & create basic components structure

---
+++ src/js/.eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es6": true
+  },
+  "extends": "eslint:recommended",
+  "parserOptions": {
+    "ecmaVersion": 2015,
+    "sourceType": "module"
+  },
+  "rules": {
+    "linebreak-style": ["off"],
+    "no-console": ["off"]
+  },
+  "globals": {
+    "Handlebars": false
+  }
+};
